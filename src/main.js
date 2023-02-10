731215d0d90b2ee380b6096a26faa174199ed369
Elson Mateus <111660816+ElsonMateusFE@users.noreply.github.com>
2023-02-10 18:56:24 -0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+// Função de Multiplicação
+function multiplicaoDoiValores(valor1, valor2) {
+    return valor1 * valor2;
+}
+// Função de Saudação
+function saudacao(nome) {
+    console.log(`Olá ${nome}`);
+}
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+// Função de Multiplicação
+function multiplicaoDoiValores(valor1: number, valor2: number): number{
+    return valor1 * valor2
+}
+
+// Função de Saudação
+function saudacao(nome: string): void{
+    console.log(`Olá ${nome}`)
+}
